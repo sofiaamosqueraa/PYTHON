3ebf41e7d6e944ba2eeb9b7ba08680bf2f865980
sofiaamosqueraa <sofiaam0202@gmail.com>
2024-09-24 12:42:10 +0100

    error apos do pull

--- app.py
+++ app.py
@@ -8,48 +8,51 @@ def home():
 
 @app.route('/documentacao')
 def documentacao():
-    return render_template('documentacao/documentacao.html')
+    return render_template('documentacao.html')
 
 @app.route('/documentacao/microservicos')
 def microservicos():
-    return render_template('documentacao/microservicos.html')
+    return render_template('microservicos.html')
 
 @app.route('/documentacao/gateway')
 def gateway():
-    return render_template('documentacao/gateway.html')
+    return render_template('gateway.html')
 
 @app.route('/documentacao/api')
 def api():
-    return render_template('documentacao/api.html')
+    return render_template('api.html')
 
 @app.route('/modulos')
 def modulos():
-    return render_template('modulos/modulos.html')
+    return render_template('modulos.html')
 
 @app.route('/modulos/cpp')
 def cpp():
-    return render_template('modulos/cpp.html')
+    return render_template('cpp.html')
 
 @app.route('/modulos/python')
 def python():
-    return render_template('modulos/python.html')
+    return render_template('python.html')
 
 @app.route('/modulos/java')
 def java():
-    return render_template('modulos/java.html')
+    return render_template('java.html')
 
 @app.route('/modulos/redes')
 def redes():
-    return render_template('modulos/redes.html')
+    return render_template('redes.html')
 
 @app.route('/modulos/banco-de-dados')
 def banco_de_dados():
-    return render_template('modulos/banco_de_dados.html')
+    return render_template('banco_de_dados.html')
 
 @app.route('/modulos/processamento')
 def processamento():
-    return render_template('modulos/processamento.html')
+    return render_template('processamento.html')
 
+@app.route('/interface')
+def interface():
+    return render_template('interface.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
