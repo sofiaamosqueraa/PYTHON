376d9044e382fcf6e252dd4fcf0cb9ad02773f87
oxyzinha <146823768+oxyzinha@users.noreply.github.com>
2024-11-27 12:13:59 +0000

    styles e informação de modulos

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-from flask import Flask, render_template
-=======
 from flask import Flask, render_template, request
->>>>>>> 781f514bae633f516a2795f644e735cfb2d90a1c
 
 app = Flask(__name__)
 
@@ -10,16 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-<<<<<<< HEAD
-@app.route('/documentacao')
-def documentacao():
-    return render_template('documentacao.html')
-=======
 @app.route('/documentacao', methods=['GET', 'POST'])
 def documentacao():
     section_to_show = request.args.get('section')  # Não define um padrão
     return render_template('documentacao.html', section=section_to_show)
->>>>>>> 781f514bae633f516a2795f644e735cfb2d90a1c
 
 @app.route('/modulos')
 def modulos():
@@ -28,6 +18,10 @@ def modulos():
 @app.route('/projeto')
 def projeto():
     return render_template('projeto.html')
+
+@app.route('/modulo/<nome>')
+def modulo(nome):
+    return render_template('modulos.html', modulo=nome)
 
 if __name__ == '__main__':
     app.run(debug=True)
